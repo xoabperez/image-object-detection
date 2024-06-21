8d5c8bb1939a998207090b8ad584bc21d7f2dc73
XP <35434658+xoabperez@users.noreply.github.com>
2024-06-21 00:27:25 -0500

    Spring Boot

---
+++ src/main/java/com/xoab/ImageObjectDetection/ImageObjectDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.xoab.ImageObjectDetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageObjectDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageObjectDetectionApplication.class, args);
+	}
+
+}
